88e2d7d97ace70f8c62960c7cf8d178e3caa4767
Sándor Czettner <sandor@czettner.hu>
2016-08-28 13:36:07 +0100

    NewsAdapter

---
+++ app/src/main/java/com/czettner/hypothesis/News.java
@@ -0,0 +1,53 @@
+package com.czettner.hypothesis;
+
+import java.util.Date;
+
+public class News {
+    private String title;
+    private String description;
+    private String category;
+    private String author;
+    private Date publishedDate;
+
+    // BEGIN Getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+    // END Getters and setters
+}
---
+++ app/src/main/java/com/czettner/hypothesis/NewsAdapter.java
@@ -0,0 +1,51 @@
+package com.czettner.hypothesis;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class NewsAdapter extends ArrayAdapter<News> {
+
+    private final static String DATE_FORMAT = "dd/MM/yyyy HH:mm";
+
+    /**
+     * Constructor
+     * @param context   Context
+     * @param news      News
+     */
+    public NewsAdapter(Context context, ArrayList<News> news) {
+        super(context, 0, news);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        News news = getItem(position);
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.title);
+        TextView tvDescription = (TextView) convertView.findViewById(R.id.description);
+        TextView tvCategory = (TextView) convertView.findViewById(R.id.category);
+        TextView tvAuthor = (TextView) convertView.findViewById(R.id.author);
+        TextView tvPublishedDate = (TextView) convertView.findViewById(R.id.published_date);
+
+        tvTitle.setText(news.getTitle());
+        tvDescription.setText(news.getDescription());
+        tvCategory.setText(news.getCategory());
+        tvAuthor.setText(news.getAuthor());
+
+        SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
+        tvPublishedDate.setText(formatter.format(news.getPublishedDate()));
+
+        return convertView;
+    }
+}
